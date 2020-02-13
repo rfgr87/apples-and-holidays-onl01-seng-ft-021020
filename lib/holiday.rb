80631e2f0bd6acfab823e259b8ed3f9f8e42d58e
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-13 21:13:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,9 +63,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts season.to_s.capitalize+":"
+    puts season.to_s.capitalize+":" + 
     holiday.each do |day, supplys|
-      puts day.to_s.capitalize+":" 
+      puts day.to_s.capitalize+":" +
       supplys.each do |x|
         puts x
         end
