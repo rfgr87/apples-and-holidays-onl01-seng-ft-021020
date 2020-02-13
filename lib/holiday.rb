c76840c29f3cc5290f11360360a15eaf403a8fcc
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-13 22:16:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,7 +94,7 @@ def all_holidays_with_bbq(holiday_hash)
   array = []
   holiday_hash.each do |season, holiday|
     holiday.each do |day, supplys| 
-      put "#{days}"
+      puts "#{day}"
     end
   end
 end
